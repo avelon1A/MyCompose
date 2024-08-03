a14c73cdb036eb6851ecc11d78c37be044c085e4
Avelon1A <amanavelon@gmail.com>
2024-08-03 16:26:51 +0530

    testing

--- app/src/main/java/com/bosch/composewithkotlin20/presentaion/ui/screen/BottomNavigationBar.kt
+++ app/src/main/java/com/bosch/composewithkotlin20/presentaion/ui/screen/BottomNavigationBar.kt
@@ -71,8 +71,6 @@ fun BottomNavigationBar() {
 	var selected by rememberSaveable { mutableIntStateOf(0) }
 	Scaffold(
 		bottomBar = {
-
-
 			NavigationBar(containerColor = MaterialTheme.colorScheme.primary,
 				contentColor = MaterialTheme.colorScheme.tertiary,){
 				items.forEachIndexed { index, item ->
