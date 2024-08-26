e2eeaea18e169fff03b0b1bdae10f3c4e3edca18
Avelon1A <amanavelon@gmail.com>
2024-08-26 16:46:08 +0530

    type safe navigation edit

--- app/src/main/java/com/bosch/composewithkotlin20/presentaion/ui/screen/TypeSafeNavigation.kt
+++ app/src/main/java/com/bosch/composewithkotlin20/presentaion/ui/screen/TypeSafeNavigation.kt
@@ -6,7 +6,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
@@ -14,19 +19,24 @@ import kotlinx.serialization.Serializable
 
 @Composable
 fun TypeSafeNavigation(navController: NavController) {
+    var text by remember { mutableStateOf("type here") }
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        OutlinedButton(onClick = { navController.navigate(TypeSafeNavigationSecond("aman")) }) {
+
+        TextField(value = text,
+            onValueChange = { newText -> text = newText })
+    }
+
+    OutlinedButton(onClick = { navController.navigate(TypeSafeNavigationSecond(text)) }) {
             Text(text = "click")
 
         }
     }
 
-}
 
 @Serializable
 object TypeSafeNavigation
