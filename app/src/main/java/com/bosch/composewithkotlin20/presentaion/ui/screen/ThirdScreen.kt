49aa06f271662fcd996dbe09badde6f83d45e6bd
aman <amanavelon@gmail.com>
2024-09-26 10:21:16 +0530

    git ph token updated

--- app/src/main/java/com/bosch/composewithkotlin20/presentaion/ui/screen/ThirdScreen.kt
+++ app/src/main/java/com/bosch/composewithkotlin20/presentaion/ui/screen/ThirdScreen.kt
@@ -1,9 +1,7 @@
 package com.bosch.composewithkotlin20.presentaion.ui.screen
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
