59530654e241dabf22aa6a7b2e17bc82ca164122
aman <amanavelon@gmail.com>
2024-10-09 18:17:02 +0530

    move

--- app/src/main/java/com/bosch/composewithkotlin20/presentaion/ui/screen/basicUI/Seekbar.kt
+++ app/src/main/java/com/bosch/composewithkotlin20/presentaion/ui/screen/basicUI/Seekbar.kt
@@ -1,9 +1,8 @@
-package com.bosch.composewithkotlin20.presentaion.ui.screen
+package com.bosch.composewithkotlin20.presentaion.ui.screen.basicUI
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,7 +12,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import kotlinx.serialization.Serializable
 
 @Composable
