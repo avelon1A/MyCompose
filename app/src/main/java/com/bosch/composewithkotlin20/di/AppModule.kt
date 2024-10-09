9d8639165eedd57593290f4f7bf6bf2679574ba3
aman <amanavelon@gmail.com>
2024-10-09 18:08:49 +0530

    Websocket

--- app/src/main/java/com/bosch/composewithkotlin20/di/AppModule.kt
+++ app/src/main/java/com/bosch/composewithkotlin20/di/AppModule.kt
@@ -28,6 +28,7 @@ import com.bosch.composewithkotlin20.presentaion.ui.viewModel.LoginViewModel
 import com.bosch.composewithkotlin20.presentaion.ui.viewModel.MainViewModel
 import com.bosch.composewithkotlin20.presentaion.ui.viewModel.OnBoardingViewModel
 import com.bosch.composewithkotlin20.presentaion.ui.viewModel.SwipeToDeleteViewModel
+import com.bosch.composewithkotlin20.presentaion.ui.viewModel.WebSocketViewModel
 import com.bosch.composewithkotlin20.util.Const
 import com.bosch.composewithkotlin20.util.NetworkHelper
 import com.bosch.composewithkotlin20.util.ServiceStarter
@@ -47,94 +48,99 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 val appModule = module {
 
-	single { provideRetrofit() }
-	single { provideSupaBaseClient() }
-	single { provideApiService(get()) }
-	single<LocalUserManager> { LocalUserMangerImp(androidApplication()) }
-	single { provideAudioAttributes() }
-	single { AudioNotificationManager(androidContext(), get()) }
-	single { JetAudioServiceHandler(get()) }
-	single { ServiceStarter(androidContext()) }
-	single { ContentResolverHelper(androidContext()) }
-	single { AudioRepository(get()) }
-	single { SaveAppEntry(get()) }
-	single { GetAppEntry(get()) }
-	single { LoginStatus(get()) }
-	single { LoginRepository(get()) }
-	single { provideExoPlayer(androidContext(), get())}
-	single { provideMediaSession(get(), get()) }
-	single { provideSupabaseRepo(get(),get(),get())}
-	single { NetworkHelper(androidApplication()) }
+    single { provideRetrofit() }
+    single { provideSupaBaseClient() }
+    single { provideApiService(get()) }
+    single<LocalUserManager> { LocalUserMangerImp(androidApplication()) }
+    single { provideAudioAttributes() }
+    single { AudioNotificationManager(androidContext(), get()) }
+    single { JetAudioServiceHandler(get()) }
+    single { ServiceStarter(androidContext()) }
+    single { ContentResolverHelper(androidContext()) }
+    single { AudioRepository(get()) }
+    single { SaveAppEntry(get()) }
+    single { GetAppEntry(get()) }
+    single { LoginStatus(get()) }
+    single { LoginRepository(get()) }
+    single { provideExoPlayer(androidContext(), get()) }
+    single { provideMediaSession(get(), get()) }
+    single { provideSupabaseRepo(get(), get(), get()) }
+    single { NetworkHelper(androidApplication()) }
 
 
-	single { AppEntryUseCase(get(), get()) }
-	viewModel { MainViewModel(get()) }
+    single { AppEntryUseCase(get(), get()) }
+    viewModel { MainViewModel(get()) }
+    viewModel { WebSocketViewModel() }
 
-	viewModel { LoginViewModel(get()) }
-	viewModel { (savedStateHandle: SavedStateHandle) ->
-		AudioViewModel(
-			get(),
-			get(),
-			get(),
-			savedStateHandle
-		)
-	}
-	viewModel { OnBoardingViewModel(get()) }
-	viewModel { SupaBaseViewModel(get()) }
-	viewModel { SwipeToDeleteViewModel() }
-	viewModel { SupabaseVideoPlayerViewModel(get()) }
+    viewModel { LoginViewModel(get()) }
+    viewModel { (savedStateHandle: SavedStateHandle) ->
+        AudioViewModel(
+            get(),
+            get(),
+            get(),
+            savedStateHandle
+        )
+    }
+    viewModel { OnBoardingViewModel(get()) }
+    viewModel { SupaBaseViewModel(get()) }
+    viewModel { SwipeToDeleteViewModel() }
+    viewModel { SupabaseVideoPlayerViewModel(get()) }
 }
 
 fun provideRetrofit(): Retrofit {
-	return Retrofit.Builder()
-		.baseUrl("https://dummyjson.com/")
-		.addConverterFactory(GsonConverterFactory.create())
-		.build()
+    return Retrofit.Builder()
+        .baseUrl("https://dummyjson.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 }
 
 fun provideApiService(retrofit: Retrofit): ApiService {
-	return retrofit.create(ApiService::class.java)
+    return retrofit.create(ApiService::class.java)
 }
 
 fun provideAudioAttributes(): AudioAttributes {
-	return AudioAttributes.Builder()
-		.setContentType(C.AUDIO_CONTENT_TYPE_MOVIE)
-		.setUsage(C.USAGE_MEDIA)
-		.build()
+    return AudioAttributes.Builder()
+        .setContentType(C.AUDIO_CONTENT_TYPE_MOVIE)
+        .setUsage(C.USAGE_MEDIA)
+        .build()
 }
 
 @OptIn(UnstableApi::class)
 fun provideExoPlayer(context: Context, audioAttributes: AudioAttributes): ExoPlayer {
-	return ExoPlayer.Builder(context)
-		.setAudioAttributes(audioAttributes, true)
-		.setHandleAudioBecomingNoisy(true)
-		.setTrackSelector(DefaultTrackSelector(context))
-		.build()
+    return ExoPlayer.Builder(context)
+        .setAudioAttributes(audioAttributes, true)
+        .setHandleAudioBecomingNoisy(true)
+        .setTrackSelector(DefaultTrackSelector(context))
+        .build()
 }
 
 fun provideMediaSession(context: Context, player: ExoPlayer): MediaSession {
-	return MediaSession.Builder(context, player).build()
+    return MediaSession.Builder(context, player).build()
 }
+
 fun provideSupaBaseClient(): SupabaseClient {
-	val supabaseUrl = Const.SUPABASE_APP_URL
-	val supabaseKey = Const.SUPABASE_API_KEY
+    val supabaseUrl = Const.SUPABASE_APP_URL
+    val supabaseKey = Const.SUPABASE_API_KEY
 
-	return createSupabaseClient(
-		supabaseKey = supabaseKey,
-		supabaseUrl = supabaseUrl
+    return createSupabaseClient(
+        supabaseKey = supabaseKey,
+        supabaseUrl = supabaseUrl
 
-	) {
-		install(Realtime)
-		install(Postgrest)
+    ) {
+        install(Realtime)
+        install(Postgrest)
 
 
+    }
+}
 
-	}
-   }
-
-	fun provideSupabaseRepo(supabaseClient: SupabaseClient,dao: CafeDao,networkHelper: NetworkHelper): SupabaseRepository {
-		return SupabaseRepoImp(supabaseClient,dao,networkHelper)
-	}
+fun provideSupabaseRepo(
+	supabaseClient: SupabaseClient,
+	dao: CafeDao,
+	networkHelper: NetworkHelper,
+): SupabaseRepository {
+    return SupabaseRepoImp(supabaseClient, dao, networkHelper)
+}
 
 
 
