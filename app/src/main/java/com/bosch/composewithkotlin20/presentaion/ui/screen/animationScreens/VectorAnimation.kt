a399a6e592672cae59d6da4f8903157ff4b9712f
aman <amanavelon@gmail.com>
2024-10-09 18:17:28 +0530

    VectorAnimation.kt text added

--- app/src/main/java/com/bosch/composewithkotlin20/presentaion/ui/screen/animationScreens/VectorAnimation.kt
+++ app/src/main/java/com/bosch/composewithkotlin20/presentaion/ui/screen/animationScreens/VectorAnimation.kt
@@ -7,15 +7,17 @@ import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
 import androidx.compose.animation.graphics.vector.AnimatedImageVector
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.bosch.composewithkotlin20.R
 import kotlinx.serialization.Serializable
 
@@ -25,16 +27,24 @@ import kotlinx.serialization.Serializable
 fun VectorAnimationScreen() {
     val image = AnimatedImageVector.animatedVectorResource(R.drawable.animated)
     var atEnd by remember { mutableStateOf(false) }
-    Image(
-        painter = rememberAnimatedVectorPainter(image, atEnd),
-        contentDescription = "Animated Vector",
-        modifier = Modifier.fillMaxSize().
-        clickable {
-            atEnd = !atEnd
-        },
-        contentScale = ContentScale.Fit
-    )
+    Box(modifier = Modifier.fillMaxSize()) {
+        Image(
+            painter = rememberAnimatedVectorPainter(image, atEnd),
+            contentDescription = "Animated Vector",
+            modifier = Modifier.fillMaxSize().clickable {
+                atEnd = !atEnd
+            },
+            contentScale = ContentScale.Fit
+        )
+        Text(
+            modifier = Modifier
+                .align(Alignment.BottomCenter).padding(bottom = 150.dp),
+            text = "Click to show",
+            fontSize = 33.sp
+        )
+    }
 }
+
 
 @Serializable
 object VectorAnimationScreen
